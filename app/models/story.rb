9519b030f88def5fdb0c6eaeedf6560de07f89b2
Ariana <ariana@astele.co.uk>
2017-04-17 21:19:39 +0100

    OD-144 Add story and bookmark meta

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,9 @@
 class Story < ApplicationRecord
   belongs_to :author
-  has_one :coauthor, foreign_key: :id, class_name: 'Author'
+  has_one :coauthor, foreign_key: :id, class_name: "Author"
   has_many :chapters
+
+  def coauthor?
+    coauthor.present?
+  end
 end
