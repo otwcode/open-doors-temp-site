ded8d3a9763005ce346e8c087818956325fe0419
Ariana <ariana@astele.co.uk>
2017-10-22 20:46:15 +0100

    OD-285 Update schema and migration file

--- db/migrate/20171021090124_add_host_to_archive_config.rb
+++ db/migrate/20171021090124_add_host_to_archive_config.rb
@@ -1,5 +1,5 @@
 class AddHostToArchiveConfig < ActiveRecord::Migration[5.0]
   def change
-    add_column :archive_configs, :host, :string
+    add_column :archive_configs, :host, :string, limit: 15, default: "ariana"
   end
 end
