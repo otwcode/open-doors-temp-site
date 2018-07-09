ea40cadbc9a28e8306dc495f1d7768f6a711fbca
Ariana <ariana@astele.co.uk>
2018-07-09 21:40:41 +0100

    OD-360 Precompile assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
   config.cache_classes = true
+  config.public_file_server.enabled = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
