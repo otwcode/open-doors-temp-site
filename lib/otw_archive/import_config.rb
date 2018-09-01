25d37a3eb668d4a1d4631c30450b7a69c066b243
Ariana <ariana@astele.co.uk>
2018-09-01 22:08:14 +0100

    OD-360 Switch to HTTPS based on Archive server location

--- lib/otw_archive/import_config.rb
+++ lib/otw_archive/import_config.rb
@@ -6,7 +6,8 @@ module OtwArchive
 
     def initialize(archive_host, token, archivist, post_without_preview = true, restricted = true,
                    override_tags = true, detect_tags = false, send_claim_emails = false, encoding = "UTF-8")
-      @archive_host = archive_host
+      protocol =  isHttp?(archive_host) ? "http" : "https"
+      @archive_host = "#{protocol}://#{archive_host}"
       @token = token
       @archivist = archivist
       @post_without_preview = post_without_preview
@@ -15,6 +16,10 @@ module OtwArchive
       @detect_tags = detect_tags
       @send_claim_emails = send_claim_emails
       @encoding = encoding
+    end
+    
+    def isHttp?(archive_host)
+      archive_host.include?("ariana.archiveofourown.org") || archive_host.include?("localhost")
     end
   end
 end # OtwArchive
