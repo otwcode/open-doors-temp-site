b3b1f6b64b78fee4ad68e689c699de9bfc42c8e1
Ariana <ariana@astele.co.uk>
2018-11-08 17:06:21 +0000

    OD-360 Move Stats page to React

--- app/javascript/config.js
+++ app/javascript/config.js
@@ -1,3 +1,2 @@
-export default {
-  sitekey: 'opendoorstempsite'
-}
+export const sitekey = 'opendoorstempsite';
+export const authors_path = (letter) => `${sitekey}/authors${ letter ? `?letter=${letter}` :""}`;
