0888645bcd8ba97d1365020c7f79fc503e886e28
Ariana <ariana@astele.co.uk>
2018-12-05 23:42:36 +0000

    OD-468 Update Rails and Sass, fix deprecation warnings and unused gems

--- spec/factories.rb
+++ spec/factories.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
     # user_with_posts will create post data after the user has been created
     factory :author_with_stories do
       transient do
-        stories_count 5
+        stories_count { 5 }
       end
 
       # the after(:create) yields two values; the user instance itself and the
@@ -31,9 +31,9 @@ FactoryBot.define do
   factory :chapter
 
   factory :archive_config do
-    key APP_CONFIG[:sitekey]
-    name APP_CONFIG[:name]
-    host "local"
+    key { APP_CONFIG[:sitekey] }
+    name { APP_CONFIG[:name] }
+    host { "local" }
     initialize_with { ArchiveConfig.where(key: key).first_or_create }
   end
 end
