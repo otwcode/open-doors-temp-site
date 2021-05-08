d335718daccfd93a7f64d2d6ae87d5e87659241c
Ariana <ariana@astele.co.uk>
2021-05-08 20:04:26 +0100

    OD-617 Fix story spec

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 describe Story, type: :model do
 
   let!(:author1) { create(:author_with_stories, audit_comment: "Test") }
-  let(:story1) { create(:story, author_id: author1.id, notes: "Original Notes", audit_comment: "Test") }
+  let(:story1) { create(:story, author_id: author1.id, notes: "Original Notes", language_code: "de", audit_comment: "Test") }
 
   it 'converts a story with all fields correctly' do
     config = create(:archive_config, stories_note: "Story note")
     work = story1.to_work(config, "test")
     expect(work.notes).to eq "Story note\nOriginal Notes"
-    expect(work.language_code).to eq "en"
+    expect(work.language_code).to eq "de"
   end
 
   it 'returns a summary too long error in json object' do
