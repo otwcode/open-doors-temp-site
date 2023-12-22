70afd03dd1ba220153d868e13212db0352b214db
Brianna Dardin <brianna.dardin@gmail.com>
2023-12-22 14:10:36 -0800

    Changed number pagination logic

--- app/javascript/components/pagination/NumberPagination.js
+++ app/javascript/components/pagination/NumberPagination.js
@@ -3,6 +3,13 @@ import Pagination from "react-bootstrap/Pagination";
 import Dropdown from "react-bootstrap/Dropdown";
 import { authors_path } from "../../config";
 
+const DOTS = "...";
+
+const range = (start, end) => {
+  let length = end - start + 1;
+  return Array.from({ length }, (_, idx) => idx + start);
+};
+
 export default class NumberPagination extends Component {
 
   handlePageChange = (e, p) => {
@@ -10,43 +17,67 @@ export default class NumberPagination extends Component {
     this.props.onPageChange(p)
   };
 
-  getShownItems = (listItems, currentPage) => {
-    const shownPages = 7;
-		const halfPages = parseInt(shownPages / 2);
-    const totalPages = listItems.length;
-		
-		let startIdx = -1;
-		let endIdx = currentPage + halfPages;
-		if (shownPages % 2 > 0) {
-			startIdx = currentPage - halfPages - 1;
-		} else {
-			startIdx = currentPage - halfPages;
-		}
-		
-		if (startIdx <= 0) {
-			return listItems.slice(1, shownPages + 1);
-		} else if (endIdx >= totalPages) {
-			return listItems.slice(totalPages - shownPages - 1, totalPages - 1);
-		} else {
-			return listItems.slice(startIdx, endIdx);
-		}
+  // Pagination logic adapted from https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
+  getPaginationRange = (currentPage, totalPageCount, siblingCount, totalPageNumbers) => {
+    if (totalPageNumbers >= totalPageCount) {
+      return range(1, totalPageCount);
+    }
+
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+    const rightSiblingIndex = Math.min(
+      currentPage + siblingCount,
+      totalPageCount
+    );
+
+    const shouldShowLeftDots = leftSiblingIndex > 2;
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+    const firstPageIndex = 1;
+    const lastPageIndex = totalPageCount;
+
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      let leftItemCount = 3 + 2 * siblingCount;
+      let leftRange = range(1, leftItemCount);
+
+      return [...leftRange, DOTS, totalPageCount];
+    }
+
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      let rightItemCount = 3 + 2 * siblingCount;
+      let rightRange = range(
+        totalPageCount - rightItemCount + 1,
+        totalPageCount
+      );
+      return [firstPageIndex, DOTS, ...rightRange];
+    }
+
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
+    }
   }
 
   render() {
     const currentPageInt = parseInt(this.props.page);
-    const pageList = [...Array(this.props.pages).keys()].map(p => p + 1);
-    
-    const listItems = pageList.map((p) => {
+    const totalPagesInt = parseInt(this.props.pages);
+    const siblingCount = 3;
+    const totalPageNumbers = siblingCount + 5;
+    const paginationRange = this.getPaginationRange(currentPageInt, totalPagesInt, siblingCount, totalPageNumbers);
+
+    let dotCount = 0;
+    const shownItems = paginationRange.map(p => {
+        if (p === DOTS) {
+          dotCount++;
+          return <Pagination.Ellipsis key={`dot-${dotCount}`} />
+        }
+
         const isCurrent = (p === currentPageInt);
         const props = {
           active: isCurrent,
           onClick: e => this.handlePageChange(e, p)
         };
         return <Pagination.Item key={p} href={authors_path(this.props.letter, p)} {...props}>{p}</Pagination.Item>
-      }
-    );
-
-    const shownItems = this.getShownItems(listItems, currentPageInt);
+    });
     
     const props = p => {
       const isValid = (p > 0 && p <= this.props.pages);
@@ -57,28 +88,31 @@ export default class NumberPagination extends Component {
       }
     };
 
+    const getDropdown = () => {
+      return (
+        <Dropdown className="page-item">
+          <Dropdown.Toggle className="page-link" id="dropdown-basic-button">Jump to page...</Dropdown.Toggle>
+          <Dropdown.Menu>
+            {[...Array(totalPagesInt).keys()].map(p => p + 1).map((p) => {
+              if (p !== currentPageInt) {
+                return <Dropdown.Item key={`${p}-link`}
+                                      onClick={e => this.handlePageChange(e, p)}>{p}</Dropdown.Item>
+              }
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+      )
+    }
+    const dropdown = (totalPageNumbers >= totalPagesInt) ? "" : getDropdown();
+
     return (
       <nav aria-label="Page labels">
         <div className="text-center">
           <Pagination className="justify-content-center">
             <Pagination.Prev {...props(currentPageInt - 1)} />
-            {listItems[0]}
-            <Pagination.Ellipsis />
             {shownItems}
-            <Pagination.Ellipsis />
-            {listItems[parseInt(this.props.pages) - 1]}
             <Pagination.Next {...props(currentPageInt + 1)} />
-            <Dropdown className="page-item">
-              <Dropdown.Toggle className="page-link" id="dropdown-basic-button">Jump to page...</Dropdown.Toggle>
-              <Dropdown.Menu>
-                {pageList.map((p) => {
-                  if (p !== currentPageInt) {
-                    return <Dropdown.Item key={`${p}-link`}
-                                          onClick={e => this.handlePageChange(e, p)}>{p}</Dropdown.Item>
-                  }
-                })}
-              </Dropdown.Menu>
-            </Dropdown>
+            {dropdown}
           </Pagination>
         </div>
       </nav>
